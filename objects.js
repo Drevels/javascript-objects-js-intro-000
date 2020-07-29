202c1509f98aaf1d232418be3d61668723b087d6
danielle revels <ddaniellerrevels@aol.com>
2020-07-29 04:11:32 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,10 +1,9 @@
-var playlist = { "song" : "artist"};
-
-function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle 
-}
-
-function removeFromPlaylist(playlist, artistName){
-  delete playlist [artistName]
+var playlist = {artist: 'song'};
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName]=songTitle
   return playlist
+}
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
 }
